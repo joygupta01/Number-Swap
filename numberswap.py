c3382ce4b36d814129c23d7ed7b143ca4ee8db0a
joygupta01 <joygupta052@gmail.com>
2025-05-24 21:18:00 +0530

    Add files via upload

---
+++ numberswap.py
@@ -0,0 +1,13 @@
+a = input("enter value of a")
+b = input("enter value of b")
+c = input("enter value of c")
+
+temp = a
+a = c
+c = b
+b = temp
+
+print("value of a after swapping" ,a)
+print("value of b after swapping" ,b)
+print("value of c after swapping" ,c)
+
